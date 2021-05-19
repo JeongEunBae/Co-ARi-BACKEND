ec11032a2896ecb55d17bd01ddb2efc4e9252fb1
JeongEunBae <je_0922@naver.com>
2021-05-20 05:45:56 +0900

    [MODIFY] 프로젝트 이름 수정

---
+++ build/resources/main/static/js/app/index.js
@@ -0,0 +1,77 @@
+var main = {
+    init : function () {
+        var _this = this;
+        $('#btn-save').on('click', function () {
+            _this.save();
+        });
+
+        $('#btn-update').on('click', function () {
+            _this.update();
+        });
+
+        $('#btn-delete').on('click', function () {
+                    _this.delete();
+                });
+    },
+    save : function () {
+        var data = {
+            title : $('#title').val(),
+            author : $('#author').val(),
+            content : $('#content').val(),
+            send_time : $('#send_time').val(),
+            read_mark : $('#read_mark').val(),
+            visibility : $('#visibility').val()
+        };
+
+        $.ajax({
+            type: 'POST',
+            url: '/api/alarms',
+            dataType: 'json',
+            contentType:'application/json; charset=utf-8',
+            data: JSON.stringify(data)
+        }).done(function() {
+            alert('알림이 등록되었습니다.');
+            window.location.href = '/';
+        }).fail(function (error) {
+            alert(JSON.stringify(error));
+        });
+    },
+    update : function () {
+        var data = {
+            read_mark : $('#read_mark').val(),
+            visibility : $('#visibility').val()
+        };
+
+        var alarm_id = $('#alarm_id').val();
+
+        $.ajax({
+            type: 'PUT',
+            url: '/api/alarms/' + alarm_id,
+            dataType: 'json',
+            contentType:'application/json; charset=utf-8',
+            data: JSON.stringify(data)
+        }).done(function() {
+            alert('알림이 수정되었습니다.');
+            window.location.href = '/';
+        }).fail(function (error) {
+            alert(JSON.stringify(error));
+        });
+    },
+    delete : function () {
+        var alarm_id = $('#alarm_id').val();
+
+        $.ajax({
+            type : 'DELETE',
+            url: '/api/alarms/' + alarm_id,
+            dataType: 'json',
+            contentType:'application/json; charset=utf-8'
+        }).done(function() {
+            alert('알림이 삭제되었습니다.');
+            window.location.href = '/';
+        }).fail(function (error) {
+            alert(JSON.stringify(error));
+        });
+    }
+};
+
+main.init();
