2fd9ec08164543782d25d0af36aa9199e275f978
JeongEunBae <je_0922@naver.com>
2021-06-01 01:21:43 +0900

    [UPDATE] notices 엔티티 수정

--- src/main/java/com/cse/coari/domain/notices/Notices.java
+++ src/main/java/com/cse/coari/domain/notices/Notices.java
@@ -3,14 +3,20 @@ package com.cse.coari.domain.notices;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.ResultCheckStyle;
+import org.hibernate.annotations.SQLInsert;
 import org.springframework.stereotype.Component;
 
 import javax.persistence.*;
 
+@Setter
 @Getter
 @Entity
+//@Table(name = "notices")
 @NoArgsConstructor
 @Component
+//@SQLInsert(sql = "INSERT INTO notices(author, title, url, date) VAULES(?, ?, ?, ?)", check = ResultCheckStyle.COUNT)
 public class Notices {
 
     // Primary KEY 지정
@@ -19,30 +25,30 @@ public class Notices {
     private Long notice_id;
 
     // 데이터 사이즈 조정, null 여부 설정
-    @Column(nullable = false)
+    @Column(nullable = false, name = "title")
     private String title;
 
-//    // 데이터 형식 변경, null 여부 설정
-//    @Column(columnDefinition = "TEXT")
-//    private String content;
-
-    @Column(nullable = false)
+    @Column(nullable = false, name = "author")
     private String author;
 
     //null 여부 설정
-    @Column(nullable = false)
+    @Column(nullable = false, name = "date")
     private String date;
 
     // 데이터 사이즈 조정, null 여부 설정
-    @Column(nullable = false)
+    @Column(nullable = false, name = "url")
     private String url;
 
     @Builder
-    public Notices(String title, /*String content,*/ String author, String date, String url) {
+    public Notices(String title, String author, String date, String url) {
         this.title = title;
-//        this.content = content;
         this.author = author;
         this.date = date;
         this.url = url;
+    }
+
+    @Override
+    public String toString() {
+        return author + ", " + title + ", " + date + ", " + url;
     }
 }
