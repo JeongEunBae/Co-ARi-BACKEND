a64707d367bb7c6a0f26e2fc5de4fd8ae454cef9
JeongEunBae <je_0922@naver.com>
2021-06-07 07:31:01 +0900

    [MODIFY] graduate 수정

--- src/main/resources/static/js/app/company.js
+++ src/main/resources/static/js/app/company.js
@@ -2,7 +2,7 @@ var main = {
 
     init : function () {
         var _this = this;
-        $('#btn-save').on('click', function () {
+        $('#btn-graduate-save').on('click', function () {
             _this.save();
         });
 
@@ -26,7 +26,7 @@ var main = {
              contentType:'application/json; charset=utf-8',
              data: JSON.stringify(data)
          }).done(function() {
-             alert('교수 정보가 등록되었습니다.');
+             alert('졸업생 정보가 등록되었습니다.');
              window.location.href = '/';
          }).fail(function (error) {
              alert(JSON.stringify(error));
